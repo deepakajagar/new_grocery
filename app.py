df74fa338b8ea42cc03b934fd2aa6c8f46016a57
deepa <deepanjalikajagar2000@gmail.com>
2020-08-02 08:34:39 +0530

    second time

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
 	# return render_template('index.html')
 	if request.method=='POST':
 		if request.form['Login']:
-			return redirect(url_for('login'))
+			return redirect(url_for('loginlink'))
 		if request.form['Sign Up']:
 			return redirect(url_for('signup'))
 		if request.form['Admin']:
@@ -36,11 +36,16 @@ def main():
 
 
 
+@app.route('/loginlink',methods = ['GET','POST'])
+def loginlink():
+	return render_template('login.html')
+
 #storing signup details in db
-@app.route('/login',methods = ['GET','POST'])
-def login():
+@app.route('/signup',methods = ['GET','POST'])
+def signup():
 	connection,cursor=connect()
-	print("enetred login")
+	print("enetred signup")
+	error = None
 	if request.method=='POST':
 		inp = request.form
 		name = inp['Username']
@@ -50,20 +55,46 @@ def login():
 		cursor.execute("select email from user where email=%s",(str(email),))
 		data = cursor.fetchall()
 		if data:
-			return print("email already exists")
+			return redirect(url_for('loginlink'))
 		else:
-			querry = "insert into user values(%s,%s,%s,%s,%s)"
+			querry = "insert into user values(%s,%s,%s,%s)"
 			cursor.execute(querry,(name,password,email,phone))
 			connection.commit()
 			connection.close()	
-			return print("inserted successfully")
-	return render_template('index.html')
+			print("inserted")
+			return redirect(url_for('login'))
+	return render_template('signup.html')
 
-@app.route('/index/')
+
+
+#checking if usn matched from database else return signup page
+@app.route('/login',methods = ['GET','POST'])
+def login():
+	connection,cursor=connect()
+	if request.method=='POST':
+		inp = request.form
+		name = inp['Username']
+		pas = inp['Password']
+		cursor = connection.cursor()
+		querry = "select password from user where username=%s"
+		cursor.execute(querry,(name,))
+		result = cursor.fetchall()
+		if result: 
+			tempp = str(result[0]['password'])
+			if tempp == str(pas):
+				print("password matched")
+				return redirect(url_for('index'))
+			else:
+				return redirect(url_for('login'))
+		else:
+			return redirect(url_for('signup'))
+	connection.close()
+	return render_template('login.html')	
+
+
+@app.route('/index',methods = ['GET','POST'])
 def index():
-	return print("index opened")
-
-
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
